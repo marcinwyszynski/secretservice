47ae1a240b3393f41193f87894be696b505b7683
Marcin Wyszynski <marcin.pixie@gmail.com>
2018-11-03 21:29:12 +0100

    Use `envconfig` (#2)

--- handler/handler.go
+++ handler/handler.go
@@ -11,20 +11,18 @@ import (
 	"github.com/pkg/errors"
 )
 
-type gqlRequest struct {
-	OperationName string                 `json:"operationName"`
-	Query         string                 `json:"query"`
-	Variables     map[string]interface{} `json:"variables"`
-}
-
+// Handler wraps a GraphQL schema to interface with AWS API Gateway.
 type Handler struct {
 	schema *graphql.Schema
 }
 
+// New returns an instance of a Handler.
 func New(schema *graphql.Schema) *Handler {
 	return &Handler{schema: schema}
 }
 
+// Handle serves as a main Lambda handler, translating API Gateway requests
+// to GraphQL, and GraphQL responses back to API Gateway ones.
 func (h *Handler) Handle(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	var ret events.APIGatewayProxyResponse
 	data, err := h.handle(ctx, event.Body)
@@ -39,11 +37,16 @@ func (h *Handler) Handle(ctx context.Context, event events.APIGatewayProxyReques
 }
 
 func (h *Handler) handle(ctx context.Context, input string) ([]byte, error) {
-	var gql gqlRequest
-	if err := json.NewDecoder(strings.NewReader(input)).Decode(&gql); err != nil {
+	var req struct {
+		OperationName string                 `json:"operationName"`
+		Query         string                 `json:"query"`
+		Variables     map[string]interface{} `json:"variables"`
+	}
+
+	if err := json.NewDecoder(strings.NewReader(input)).Decode(&req); err != nil {
 		return nil, errors.Wrap(err, "could not unmarshal request")
 	}
-	ret, err := json.Marshal(h.schema.Exec(ctx, gql.Query, gql.OperationName, gql.Variables))
+	ret, err := json.Marshal(h.schema.Exec(ctx, req.Query, req.OperationName, req.Variables))
 	if err != nil {
 		return nil, errors.Wrap(err, "could not write back the reponse")
 	}
